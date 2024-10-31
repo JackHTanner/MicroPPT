bf89da1b9ca1ff8834eab9696c98c44f4388e9b4
JackHTanner <jackhtanner@arizona.edu>
2024-10-30 18:54:57 -0700

    JACKHTANNER

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+//#include <iostream>
+
+// put function declarations here:
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);  
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello world!");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+// put function definitions here:
